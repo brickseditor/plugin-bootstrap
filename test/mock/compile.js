f8ce4c19f0a2c4a1b049f9b225f32ec2e3dc3140
Aymar Sossou <saymar90@gmail.com>
2013-12-15 14:16:43 +0100

    Used single angular element object instead of creating two in compile mock

--- test/mock/compile.js
+++ test/mock/compile.js
@@ -4,9 +4,9 @@ angular.module('compileMock', [])
   .service('compile', function ($compile, $templateCache) {
 
     return function (component) {
-      var xml = $templateCache.get(component);
-      var options = angular.element(xml).find('options').html().trim();
-      var script = angular.element(xml).find('script').html().trim();
+      var xml = angular.element($templateCache.get(component));
+      var options = xml.find('options').html().trim();
+      var script = xml.find('script').html().trim();
       var Script;
 
       (new Function('register', script))(function (name, script) { // jshint ignore:line
